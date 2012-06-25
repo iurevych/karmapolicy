4a12b9adfaeb381f3d60b6e634062ba1640858a7
iurevych <sergii.iurevych@railsware.com>
2012-06-25 09:23:57 +0300

    Initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Karmapolicy::Application.config.secret_token = '16054bb3123a513269f25b4bf761e031835f7707ef03bf090c04b58d7ca47b224a34f5e307af51eac7b5f1e7fcef5f75b2981a2713a00c09e8fb1bc4d84a727d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Karmapolicy::Application.config.session_store :cookie_store, :key => '_karmapolicy_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Karmapolicy::Application.config.session_store :active_record_store
